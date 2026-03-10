5dd930b465a664dd37dab644b1c815d67aac948b
Boya Devaarya <boyadevaarya@gmail.com>
2026-03-11 00:17:16 +0530

    Completed demo1

---
+++ level1/demo1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void input(int n,float arr[n])
+{
+    printf("Enter the float values\n:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%f",&arr[i]);
+    }
+}
+int find_max_index(int n,float arr[n])
+{
+    int max_index=0;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>arr[max_index])
+        {
+            max_index=i;
+        }
+        return max_index;
+    }
+}
+void output(float arr[],int max_index)
+{
+    printf("The maximum element is %.2f\n",arr[max_index]);
+    printf("The maximum element index is %d\n",max_index);
+}
+int main()
+{
+    int n,max_index;
+    printf("Enter the number of elments:");
+    scanf("%d",&n);
+    float arr[n];
+    input(n,arr);
+    max_index=find_max_index(n,arr);
+    output(arr,max_index);
+    return 0;
+
+}
